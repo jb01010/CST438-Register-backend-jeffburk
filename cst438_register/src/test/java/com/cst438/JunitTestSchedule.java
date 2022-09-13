42fe8d039937170c44e81dd2732caceba889ecbf
J <J@DESKTOP>
2022-09-13 11:54:16 -0700

    Fixed type

--- cst438_register/src/test/java/com/cst438/JunitTestSchedule.java
+++ cst438_register/src/test/java/com/cst438/JunitTestSchedule.java
@@ -336,6 +336,7 @@ public class JunitTestSchedule {
         StudentDTO result = fromJsonString(response.getContentAsString(), StudentDTO.class);
         assertEquals(0, result.status_code);
         verify(studentRepository).save(any(Student.class));
+        
     }
 
 
